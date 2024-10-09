2f80a60ba43c8ba57db3ee1fc39a738a78f68c9e
Ryan Woodzell <ryanwoodzell@gmail.com>
2024-10-09 19:20:18 -0400

    cleaned

--- fileReader/src/Main.java
+++ fileReader/src/Main.java
@@ -23,26 +23,31 @@ public class Main {
         readFile f = new readFile();
         readFile g = new readFile();
 
+        //read and clean files from their paths
 
         ArrayList<String> electionArticle1= a.readAndClean("fileReader/src/Topic1Election/electionArticle1.txt");
-        basicStatistics z = new basicStatistics(a.getFile(), a.getCleanedFile());
-        System.out.println(a.getCleanedFile());
-        z.readStatistics();
-        /*ArrayList<String> electionarticle1 = a.readInstanceFile("fileReader/src/Topic1Election/electionArticle1.txt");
-        ArrayList<String> electionarticle2 = b.readInstanceFile("fileReader/src/Topic1Election/electionArticle2.txt");
-        ArrayList<String> electionarticle3 = c.readInstanceFile("fileReader/src/Topic1Election/electionArticle3.txt");
-        ArrayList<String> olympicsarticle1 = d.readInstanceFile("fileReader/src/Topic2Olympics/OlympicArticle1.txt");
-        ArrayList<String> olympicsarticle2 = e.readInstanceFile("fileReader/src/Topic2Olympics/OlympicArticle2.txt");
-        ArrayList<String> olympicsarticle3 = f.readInstanceFile("fileReader/src/Topic2Olympics/OlympicArticle3.txt");
+        ArrayList<String> electionArticle2 = b.readAndClean("fileReader/src/Topic1Election/electionArticle2.txt");
+        ArrayList<String> electionArticle3 = c.readAndClean("fileReader/src/Topic1Election/electionArticle3.txt");
+        ArrayList<String> olympicsArticle1 = d.readAndClean("fileReader/src/Topic2Olympics/OlympicsArticle1.txt");
+        ArrayList<String> olympicsArticle2 = e.readAndClean("fileReader/src/Topic2Olympics/OlympicArticle2.txt");
+        ArrayList<String> olympicsArticle3 = f.readAndClean("fileReader/src/Topic2Olympics/OlympicArticle3.txt");
+
+        //Gather Basic Statistics from each file
+
+        basicStatistics E1 = new basicStatistics(a.getFile(), a.getCleanedFile());
+        /*basicStatistics E2 = new basicStatistics(b.getFile(), b.getCleanedFile());
+        basicStatistics E3 = new basicStatistics(c.getFile(), c.getCleanedFile());
+        basicStatistics O4 = new basicStatistics(d.getFile(), d.getCleanedFile());
+        basicStatistics O5 = new basicStatistics(e.getFile(), e.getCleanedFile());
+        basicStatistics O6 = new basicStatistics(f.getFile(), f.getCleanedFile());
+        */
+        E1.readStatistics();
+
+        /*
+
 
         //Read and clean imported files
-        a.readInstanceFile("fileReader/src/Topic1Election/electionArticle1.txt");
-        b.readInstanceFile("fileReader/src/Topic1Election/electionArticle2.txt");
-        c.readInstanceFile("fileReader/src/Topic1Election/electionArticle3.txt");
-        d.readInstanceFile("fileReader/src/Topic2Olympics/OlympicArticle1.txt");
-        e.readInstanceFile("fileReader/src/Topic2Olympics/OlympicArticle2.txt");
-        f.readInstanceFile("fileReader/src/Topic2Olympics/OlympicArticle3.txt");
-        ArrayList<String> removedWords = a.removeStopWords();
+
         //frequencies(removedWords);
         //int uniqueW = uniqueWords(textWords);
         //System.out.println(uniqueW);
--- fileReader/src/basicStatistics.java
+++ fileReader/src/basicStatistics.java
@@ -6,29 +6,133 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class basicStatistics {
-    ArrayList<String> withstopwords;
-    ArrayList<String> withoutstopwords;
+    ArrayList<String> withStopWords;
+    ArrayList<String> withoutStopWords;
     int uniquewords;
+    String[] cleanedArray;
+    int[] cleanedArrayIndex;
 
-    public basicStatistics(ArrayList<String> withstopwords, ArrayList<String> withoutstopwords) {
-        this.withstopwords = withstopwords;
-        this.withoutstopwords = withoutstopwords;
-        this.uniquewords = uniquewords;
+    //Constructor for statistics reader
+
+    public basicStatistics(ArrayList<String> withStopWords, ArrayList<String> withoutStopWords) {
+
+        this.withStopWords = withStopWords;
+        this.withoutStopWords = withoutStopWords;
+        countFreq(listToArray(withoutStopWords), withoutStopWords.size());
+        for (int i = 0; i < cleanedArray.length; i++) {
+            System.out.println(cleanedArrayIndex[i]);
+            System.out.println(cleanedArray[i]);
+        }
     }
+
+    //Print out the basicStatistics of the file
 
     public void readStatistics() {
-        System.out.println("The number of words in the article before removing stopwords is " + withstopwords.size());
-        System.out.println("The number of words in the article after removing stopwords is " + withoutstopwords.size());
-        uniqueWords(withoutstopwords);
+        System.out.println("The number of words in the article before removing stopwords is " + withStopWords.size());
+        System.out.println("The number of words in the article after removing stopwords is " + withoutStopWords.size());
+        uniqueWords(withoutStopWords);
         System.out.println("The number of unique words in the article is " + uniquewords);
     }
+
+    //Count the unique words of file
+
     public void uniqueWords(ArrayList<String> words){
         Object[] object = words.toArray();
-        //need to implement a if statement to make everything in array lowercase
         for (int i = 0; i < object.length; i++) {
             object[i] = object[i].toString().toLowerCase();
         }
         Set result = new HashSet(Arrays.asList(object));
         uniquewords = result.size();
+    }
+
+    //Count how many words repeat.
+    //Takes in array of words and length of array
+    //need to debug countFreq
+    public  void countFreq(String[] arr, int n) {
+        boolean[] visited = new boolean[n];
+        int[] Freq = new int[n];
+
+        Arrays.fill(visited, false);
+
+        // Traverse through array elements and
+        // count frequencies
+
+        for (int i = 0; i < n; i++) {
+
+            // Skip this element if already processed
+
+            if (visited[i])
+                continue;
+
+            // Count frequency
+
+            int count = 1;
+
+            for (int j = 0; j < n; j++) {
+                if (arr[i].equalsIgnoreCase(arr[j])){
+                    visited[j] = true;
+                    count++;
+                }
+
+                //Add count to indexArray, which records how many instances of a certain string
+                Freq[j] = count;
+            }
+
+        }
+
+
+        //create cleanedArray
+
+        this.cleanedArray = arr;
+        this.cleanedArrayIndex = Freq;
+
+        //Sort the arrays by frequency they occur in article.
+        bubbleSort(cleanedArray.length);
+    }
+
+    //Convert the arrayList to a string array for the frequency counter
+
+    public String[] listToArray(ArrayList<String> words){
+        String[] str = new String[words.size()];
+
+        for (int i = 0; i < words.size(); i++) {
+            str[i] = words.get(i);
+        }
+        return(str);
+
+    }
+
+    //bubble sort the arrays in countFreq
+
+    public void bubbleSort(int n) {
+        int i, j, temp;
+        String temp2;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (cleanedArrayIndex[j] > cleanedArrayIndex[j + 1]) {
+
+                    // Swap cleanedArrayIndex[j] and cleanedArrayIndex[j+1]
+
+                    temp = cleanedArrayIndex[j];
+                    cleanedArrayIndex[j] = cleanedArrayIndex[j + 1];
+                    cleanedArrayIndex[j + 1] = temp;
+                    swapped = true;
+
+                    //Swap cleanedArray[j] and cleanedArray[j+1
+
+                    temp2 = cleanedArray[j];
+                    cleanedArray[j] = cleanedArray[j + 1];
+                    cleanedArray[j + 1] = temp2;
+
+                }
+            }
+
+            // If no two elements were
+            // swapped by inner loop, then break
+            if (swapped == false)
+                break;
+        }
     }
 }
