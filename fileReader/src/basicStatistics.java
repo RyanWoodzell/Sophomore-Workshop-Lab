a8cf768a4d6a91f75f82ff714daf6dd5c8bbc3a7
Ryan Woodzell <ryanwoodzell@gmail.com>
2024-10-09 14:28:38 -0400

    bug fix

--- fileReader/src/basicStatistics.java
+++ fileReader/src/basicStatistics.java
@@ -6,9 +6,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class basicStatistics {
-    static ArrayList<String> withstopwords;
-    static ArrayList<String> withoutstopwords;
-    static int uniquewords;
+    ArrayList<String> withstopwords;
+    ArrayList<String> withoutstopwords;
+    int uniquewords;
 
     public basicStatistics(ArrayList<String> withstopwords, ArrayList<String> withoutstopwords) {
         this.withstopwords = withstopwords;
@@ -16,13 +16,13 @@ public class basicStatistics {
         this.uniquewords = uniquewords;
     }
 
-    public static void readStatistics() {
+    public void readStatistics() {
         System.out.println("The number of words in the article before removing stopwords is " + withstopwords.size());
         System.out.println("The number of words in the article after removing stopwords is " + withoutstopwords.size());
         uniqueWords(withoutstopwords);
         System.out.println("The number of unique words in the article is " + uniquewords);
     }
-    public static void uniqueWords(ArrayList<String> words){
+    public void uniqueWords(ArrayList<String> words){
         Object[] object = words.toArray();
         //need to implement a if statement to make everything in array lowercase
         for (int i = 0; i < object.length; i++) {
