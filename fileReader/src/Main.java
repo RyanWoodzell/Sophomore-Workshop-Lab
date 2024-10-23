8e20416f75710e605f6e132ce0f10cf2d94b6232
Ryan Woodzell <ryanwoodzell@gmail.com>
2024-10-23 11:46:14 -0400

    clean

--- fileReader/src/Main.java
+++ fileReader/src/Main.java
@@ -14,52 +14,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         FileReaderEJR a = new FileReaderEJR();
         a.run();
-        /*
-        //Create file objects
-
-        readFile a = new readFile();
-        readFile b = new readFile();
-        readFile c = new readFile();
-        readFile d = new readFile();
-        readFile e = new readFile();
-        readFile f = new readFile();
-
-        //read and clean files from their paths
-
-        ArrayList<String> electionArticle1= a.readAndClean("fileReader/src/Topic1Election/electionArticle1.txt");
-        ArrayList<String> electionArticle2 = b.readAndClean("fileReader/src/Topic1Election/electionArticle2.txt");
-        ArrayList<String> electionArticle3 = c.readAndClean("fileReader/src/Topic1Election/electionArticle3.txt");
-        ArrayList<String> olympicsArticle1 = d.readAndClean("fileReader/src/Topic2Olympics/OlympicsArticle1.txt");
-        ArrayList<String> olympicsArticle2 = e.readAndClean("fileReader/src/Topic2Olympics/OlympicArticle2.txt");
-        ArrayList<String> olympicsArticle3 = f.readAndClean("fileReader/src/Topic2Olympics/OlympicArticle3.txt");
-
-        //Gather Basic Statistics from each file
-
-        basicStatistics E1 = new basicStatistics(a.getFile(), a.getCleanedFile());
-        basicStatistics E2 = new basicStatistics(b.getFile(), b.getCleanedFile());
-        basicStatistics E3 = new basicStatistics(c.getFile(), c.getCleanedFile());
-        basicStatistics O1 = new basicStatistics(d.getFile(), d.getCleanedFile());
-        basicStatistics O2 = new basicStatistics(e.getFile(), e.getCleanedFile());
-        basicStatistics O3 = new basicStatistics(f.getFile(), f.getCleanedFile());
-
-        E1.topWords(6);
-        //output statistics to the user.
-        /*E1.readStatistics();
-        E2.readStatistics();
-        E3.readStatistics();
-        O1.readStatistics();
-        O2.readStatistics();
-        O3.readStatistics();
-
-         */
-        /*
-        E1.richestvocabulary();
-        E2.richestvocabulary();
-        E3.richestvocabulary();
-        O1.richestvocabulary();
-        O2.richestvocabulary();
-        O3.richestvocabulary();
-
-         */
+    
     }}
 
