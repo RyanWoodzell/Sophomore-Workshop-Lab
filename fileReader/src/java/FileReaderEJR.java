06d500961a13935f264f362ee405b8b9964b9a46
JackCallanan <john.callanan1@student.fairfield.edu>
2024-11-13 11:28:50 -0500

    updated user interface

--- fileReader/src/java/FileReaderEJR.java
+++ fileReader/src/java/FileReaderEJR.java
@@ -11,8 +11,8 @@ public class FileReaderEJR {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the FileReader");
         System.out.println("Would you like to read articles about 1. The Olympics or 2. The Presidential Election. (Input 1 or 2)");
-        int response = sc.nextInt();
-        if(response == 1) {
+        int topicResponse = sc.nextInt();
+            if(topicResponse == 1) {
 
             //Create read file objects
 
@@ -22,6 +22,8 @@ public class FileReaderEJR {
 
             //Read all olympic files and clean
             d.getTxt("fileReader/src/Topic2Olympics/OlympicsArticle1.txt");
+            e.getTxt("fileReader/src/Topic2Olympics/OlympicArticle2.txt");
+            f.getTxt("fileReader/src/Topic2Olympics/OlympicArticle3.txt");
             ArrayList<String> olympicsArticle1 = d.readAndClean("fileReader/src/Topic2Olympics/OlympicsArticle1.txt");
             ArrayList<String> olympicsArticle2 = e.readAndClean("fileReader/src/Topic2Olympics/OlympicArticle2.txt");
             ArrayList<String> olympicsArticle3 = f.readAndClean("fileReader/src/Topic2Olympics/OlympicArticle3.txt");
@@ -34,38 +36,47 @@ public class FileReaderEJR {
 
             System.out.println("What Would you like to know:\n1.Which article uses the richest vocabulary?\n2.See the top words repeated in each article?\n3.Sentiment Analysis");
             System.out.println("Select 1/2/3 : ");
-            response = sc.nextInt();
+            int response = sc.nextInt();
             if(response == 1) {
                 System.out.println("What amount of prevelent words would you like to see(integer):");
                 int amount = sc.nextInt();
 
                 //Richest Vocab
+                System.out.println("Article 1");
                 O1.topWords(amount);
+                System.out.println("Article 2");
                 O2.topWords(amount);
+                System.out.println("Article 3");
                 O3.topWords(amount);
             }
             else if(response == 2) {
+                System.out.println("Article 1");
                 O1.richestvocabulary();
+                System.out.println("Article 2");
                 O2.richestvocabulary();
+                System.out.println("Article 3");
                 O3.richestvocabulary();
             }
             else if(response == 3) {
                 System.out.println("Would you like to analyze article 1, 2, or 3: ");
-                response = sc.nextInt();
-                if(response == 1) {
+                int olympicSentimentResponse = sc.nextInt();
+                if(olympicSentimentResponse == 1) {
                     O1.sentimentAnalysis(d.getOriginalFile());
                 }
-                else if(response == 2) {
-                    O2.sentimentAnalysis(d.getOriginalFile());
+                else if(olympicSentimentResponse == 2) {
+                    O2.sentimentAnalysis(e.getOriginalFile());
                 }
-                else if(response == 3) {
-                    O3.sentimentAnalysis(d.getOriginalFile());
+                else if(olympicSentimentResponse == 3) {
+                    O3.sentimentAnalysis(f.getOriginalFile());
+                }
+                else{
+                    System.out.println("Please enter a number that corresponds to an article.");
                 }
 
             }
 
         }
-        if(response == 2) {
+        else if(topicResponse == 2) {
             //Read and clean all election files.
 
             readFile a = new readFile();
@@ -73,8 +84,10 @@ public class FileReaderEJR {
             readFile c = new readFile();
 
             //Read and clean all election files.
-
-            ArrayList<String> electionArticle1= a.readAndClean("fileReader/src/Topic1Election/electionArticle1.txt");
+                a.getTxt("fileReader/src/Topic1Election/electionArticle1.txt");
+                b.getTxt("fileReader/src/Topic1Election/electionArticle2.txt");
+                c.getTxt("fileReader/src/Topic1Election/electionArticle3.txt");
+            ArrayList<String> electionArticle1 = a.readAndClean("fileReader/src/Topic1Election/electionArticle1.txt");
             ArrayList<String> electionArticle2 = b.readAndClean("fileReader/src/Topic1Election/electionArticle2.txt");
             ArrayList<String> electionArticle3 = c.readAndClean("fileReader/src/Topic1Election/electionArticle3.txt");
 
@@ -86,23 +99,44 @@ public class FileReaderEJR {
 
             System.out.println("What Would you like to know:\n1.Which article uses the richest vocabulary?\n2.See the top words repeated in each article?\n3.Sentiment Analysis");
             System.out.println("Select 1/2/3 : ");
-            response = sc.nextInt();
+            int response = sc.nextInt();
             if(response == 1) {
                 System.out.println("What amount of prevelent words would you like to see(integer):");
                 int amount = sc.nextInt();
 
                 //Richest Vocab
+                System.out.println("Article 1");
                 E1.topWords(amount);
+                System.out.println("Article 2");
                 E2.topWords(amount);
+                System.out.println("Article 3");
                 E3.topWords(amount);
             }
             else if(response == 2) {
+                System.out.println("Article 1");
                 E1.richestvocabulary();
+                System.out.println("Article 2");
                 E2.richestvocabulary();
+                System.out.println("Article 3");
                 E3.richestvocabulary();
             }
-            else if(response == 3) {}
+            else if(response == 3) {
+                System.out.println("Would you like to analyze article 1, 2, or 3: ");
+                int electionSentimentResponse = sc.nextInt();
+                if(electionSentimentResponse == 1) {
+                    E1.sentimentAnalysis(a.getOriginalFile());
+                }
+                else if(electionSentimentResponse == 2) {
+                    E2.sentimentAnalysis(b.getOriginalFile());
+                }
+                else if(electionSentimentResponse == 3) {
+                    E3.sentimentAnalysis(c.getOriginalFile());
+                }
+                else{
+                    System.out.println("Please enter a number that corresponds to an article.");
+                }
 
+            }
 
         }
 
